e4415e44f978ab33621e586e2bcf326693df820c
Marwan Behillil <39554340+MarwanBHL@users.noreply.github.com>
2026-02-19 10:52:32 +0000

    fix: improve comments for clarity in invoice import tests and wizard

--- account_invoice_import_invoice2data/tests/test_invoice_import.py
+++ account_invoice_import_invoice2data/tests/test_invoice_import.py
@@ -169,7 +169,8 @@ class TestInvoiceImport(TransactionCase):
 
         # Following tests are disabled. Not yet implemented in account_invoice_import
         # self.assertEqual(inv.journal_id.payment_reference, "202309097001")
-        # self.assertEqual(inv.journal_id.incoterm_id, self.env.ref("account.incoterm_DPU")
+        # self.assertEqual(inv.journal_id.incoterm_id,
+        # self.env.ref("account.incoterm_DPU")
 
         self.assertEqual(len(inv.invoice_line_ids), 7)
         iline = inv.invoice_line_ids[0]
@@ -264,7 +265,8 @@ class TestInvoiceImport(TransactionCase):
         # This causes an UnboundLocalError when trying to access undefined variables
         line_data = {"price_total": 120.0}
 
-        # The function will raise UnboundLocalError due to undefined amount_type variable
+        # The function will raise UnboundLocalError due to undefined amount_type
+        # variable
         with self.assertRaises(UnboundLocalError):
             wizard.parse_invoice2data_taxes(line_data)
 
